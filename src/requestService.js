fd9d21bd515d3bab9d2f702bbafb376879265e6c
SanRamirez <santiago.ramiress@gmail.com>
2018-06-02 14:32:02 -0500

    cors

--- src/requestService.js
+++ src/requestService.js
@@ -1,25 +1,28 @@
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-let url = "https://codebreakerdojo.herokuapp.com/api/codebreaker/";
-
+//const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+let url = "http://localhost:5000/api/codebreaker/";
+//let url = "http://codebreakerdojo.herokuapp.com/api/codebreaker/"
+/*
 const options =  {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-  }
-
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Method': 'GET',
+    'Access-Control-Allow-Origin': '*'
+  },
+  mode: 'no-cors'
+}*/
 
 class RequestService {
-
     // async function
     async getRequest(number){
-      let data = await (await (fetch(proxyUrl + url + number,options)
+
+      let data = await (await (fetch(url + number)
         .then(res => {
-          return res.json()
+        return res.json()
         })
         .catch(err => {
-          //console.log('Error: ', err)
+          console.log('Error: ', err)
           return err;
         })
       ))
